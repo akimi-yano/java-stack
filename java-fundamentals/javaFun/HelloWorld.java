0bfbc98dddbddc18a0eccfc436a942a01f86bc36
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-04-06 00:40:14 -0700

    first projects in java

--- java-fundamentals/javaFun/HelloWorld.java
+++ java-fundamentals/javaFun/HelloWorld.java
@@ -1,5 +1,21 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // primitive types: not a class; denoted with lower case
+        int a = 1;
+        float f = 1.0f;
+        double d = 1.0;
+        boolean b = false;
+        char c = 'c';
+
+        // Class String (part of standard library)
+        String test = "this is a test string";
+        // toUpperCase() is a non-static method; it operates on the instance object
+        System.out.println(test.toUpperCase());
+
+        boolean booleanValue = false;
+        // valueOf() is a static method; it does not operate on a particular instance
+        System.out.println(String.valueOf(booleanValue));
     }
 }
